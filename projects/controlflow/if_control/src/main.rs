852e655dc4ebc5b9a3ca62ba0f19906e0a81bfd9
unknown <Pascalonaho@gmail.com>
2026-08-19 18:28:52 +0100

    updated

--- projects/controlflow/if_control/src/main.rs
+++ projects/controlflow/if_control/src/main.rs
@@ -1,11 +1,22 @@
-fn main() {
-    let dna_base = 'A';
+use std::io;
 
-    if dna_base == 'C' {
-        println!("This is not the appropriate DNA Base");
-    } else if dna_base == 'A' {
-        println!("This is the appropriate DNA base");
+fn main() {
+    println!("You are to input a nitrogenous base of your choice:");
+    let mut dna_base = String::new();
+
+    io::stdin().read_line(&mut dna_base).expect("The input was invalid");
+    let dna_base: char = dna_base.trim().chars().next().expect("The input was invalid at this stage");
+
+    if dna_base == 'A' {
+        println!("Adenine");
+    } else if dna_base == 'G' {
+        println!("Guanine");
+    } else if dna_base == 'T' {
+        println!("Thymine");
+    } else if dna_base == 'C' {
+        println!("Cytosine");
     } else {
-        println!("Wrong DNA Base!");
+        println!("Not a nitrogenous base");
     }
+
 }
