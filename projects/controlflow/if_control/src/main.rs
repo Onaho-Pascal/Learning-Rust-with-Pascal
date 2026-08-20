461ac4e76c0fb3eefa2ef3a8e199e4eb65cf6018
unknown <Pascalonaho@gmail.com>
2026-08-20 17:01:05 +0100

    updated

--- projects/controlflow/if_control/src/main.rs
+++ projects/controlflow/if_control/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let mut dna_base = String::new();
 
     io::stdin().read_line(&mut dna_base).expect("The input was invalid");
-    let dna_base: char = dna_base.trim().chars().next().expect("The input was invalid at this stage");
+    let dna_base: char = dna_base.trim().parse().expect("The input was invalid at this stage");
 
     if dna_base == 'A' || dna_base == 'G'{
         println!("Purine Base");
@@ -15,4 +15,15 @@ fn main() {
         println!("Not a nitrogenous base");
     }
 
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    println!("Input an array index of your choice:");
+    let mut index = String::new();
+
+    io::stdin().read_line(&mut index).expect("Not a valid array");
+
+    let index: usize = index.trim().parse().expect("Not a valid input");
+
+    let final_number = a[index];
+    println!("The number at index {} of array a is {}", index, final_number);
 }
